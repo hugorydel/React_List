d075e16d686ddb49a3fffe8e31a9ecff0c2aefc3
Hugo Rydel <hugosteczkowski@gmail.com>
2020-05-11 22:21:21 -0400

    Practice Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
+  let state = {
+    todos: [
+      { id: 1, title: "Take the trash out", completed: false },
+      { id: 2, title: "Dinner With Wife", completed: true },
+      { id: 3, title: "Wash Dishes", completed: false },
+    ],
+  };
+
   return (
     <div className='App'>
-      <p>React Application</p>
+      <Todos todos={state.todos} />
     </div>
   );
 }
